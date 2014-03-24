6df427c2cf2862c5b30ccd0e487a604e8b6fd6ac
Zachary Clute <zachclute@gmail.com>
2014-03-23 22:36:47 -0400

    Changed to eq

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 'On');
 require_once $_SERVER['DOCUMENT_ROOT']."/resources/scripts/classes/html_class.php";
 $H = new Html();
 
-if($_SERVER['HTTP_REFERER'] = "andrewcarnes.com")
+if($_SERVER['HTTP_REFERER'] eq "andrewcarnes.com")
 	header("Location: http://www.reddit.com");
 //output standard template
 $H->template($attributes = array('head','logo','front_page','footer'));
