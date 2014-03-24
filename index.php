519b3d9fe35bc49aa34621f00c23509fa1491de1
Zachary Clute <zachclute@gmail.com>
2014-03-23 22:28:11 -0400

    no message

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 'On');
 require_once $_SERVER['DOCUMENT_ROOT']."/resources/scripts/classes/html_class.php";
 $H = new Html();
 
+if($_SERVER['HTTP_REFERER'] = "andrewcarnes.com")
+	header("Location: reddit.com");
 //output standard template
 $H->template($attributes = array('head','logo','front_page','footer'));
 ?>
