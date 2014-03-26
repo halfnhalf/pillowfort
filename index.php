233ea896c888c6c756e35ec75da2afbdb29e68ce
Zachary Clute <zachclute@gmail.com>
2014-03-26 04:33:50 -0400

    Revert redirect

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 'On');
 require_once $_SERVER['DOCUMENT_ROOT']."/resources/scripts/classes/html_class.php";
 $H = new Html();
 
-header("Location: http://www.reddit.com");
 //output standard template
 $H->template($attributes = array('head','logo','front_page','footer'));
 ?>
